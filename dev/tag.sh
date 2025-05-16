921f76501e81d7086a0f479b3e9afdb1c76ca610
Henrique Alves <henrique013.a@gmail.com>
2025-05-16 16:47:58 -0300

    Refactor tag.sh

--- dev/tag.sh
+++ dev/tag.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/bash
 
-git tag "$1"
+# remove 'v' prefix from version if it exists
+version=${1#v}
 
-git push origin "$1"
+# validate version format (must be in format: 9.9.9)
+if ! [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Error: Version must be in format: X.Y.Z (e.g. 1.2.3)"
+    exit 1
+fi
+
+# tag the version
+git tag "v$version"
+
+# push the tag to the remote repository
+git push origin "v$version"
